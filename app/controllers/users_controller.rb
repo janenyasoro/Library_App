a62c0d2df0e7f5b54256e541b929f1dd76c6fd7a
janenyasoro <janenyasoro@gmail.com>
2016-12-06 23:57:44 +0300

    user can log in upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,12 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 	def show
+    @user = User.find(params[:id])
 	end
 	 def create
      @user = User.new(user_params)
    if @user.save
+       log_in @user
       flash[:success] = "Sign Up Successful"
        redirect_to users_url
     else
